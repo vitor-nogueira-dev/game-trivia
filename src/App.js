1c231c1078375ba0b164626af3133a3d361caae1
Douglas Araújo <goudxs@gmail.com>
2023-02-09 18:07:27 -0300

    feat: Adiciona um Header com as informações do player e um score zerado, à página do Game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
 import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </div>
   );
