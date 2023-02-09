f761e7ce85d6ca21f06f3f46e59391b56a158a4e
Milena Diniz Vieira <miladvieira@outlook.com>
2023-02-09 19:02:40 -0300

    re2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import validator from 'validator';
+import PropTypes from 'prop-types';
+import getToken from '../redux/actions';
 
 export default class Login extends Component {
   state = {
@@ -10,6 +12,12 @@ export default class Login extends Component {
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value }, this.handleValidated);
+  };
+
+  handleClick = async () => {
+    await getToken();
+    const { history } = this.props;
+    history.push('/game');
   };
 
   handleValidated = () => {
@@ -45,6 +53,7 @@ export default class Login extends Component {
         <button
           type="button"
           disabled={ isDisabled }
+          onClick={ this.handleClick }
           data-testid="btn-play"
         >
           Play
@@ -53,3 +62,9 @@ export default class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
