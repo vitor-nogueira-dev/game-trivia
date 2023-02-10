20f666f9b6fe57275d64d211483ce71bc3443a9b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-10 16:48:31 -0300

    feat: requisito 08 timer implementado

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,16 +6,20 @@ import { getToken } from '../helpers/localStorage';
 // import { requestTokenAPI } from '../helpers/requestApi';
 // import { requestAPI } from '../redux/actions';
 import Header from '../components/Header';
-import Timer from '../components/Timer';
 
 import Questions from '../components/Questions';
 import { saveQuestions } from '../redux/actions';
+
+const TIMER_TIME = 30;
 
 class Game extends Component {
   state = {
     questionIndex: 0,
     results: [],
     answersShuffled: [],
+    timerCounter: TIMER_TIME,
+    clicked: false,
+    isDisabled: false,
   };
 
   async componentDidMount() {
@@ -33,12 +37,47 @@ class Game extends Component {
         history.push('/');
         return;
       }
-      this.setState({ results }, this.shuffleAnswers);
+      this.setState({ results }, () => {
+        this.shuffleAnswers();
+        this.startTimer();
+      });
       dispatch(saveQuestions(results));
     } catch (error) {
       console.warn(error);
     }
   }
+
+  componentWillUnmount() {
+    // garante que o intervalo será finalizado ao desmontar o componente
+    this.stopTimer();
+  }
+
+  stopTimer = () => {
+    clearInterval(this.intervalID);
+    this.intervalID = null;
+  };
+
+  startTimer = () => {
+    const aSecondInMiliseconds = 1000;
+
+    // inicia o timer e salva o ID que será utilizado para conseguir parar
+    // A cada segundo, atualiza o estado do counter diminuindo 1
+    this.intervalID = setInterval(() => {
+      this.setState(({ timerCounter, clicked }) => {
+        if (timerCounter === 0 || clicked) {
+          this.stopTimer();
+          return { isDisabled: true };
+        }
+        return { timerCounter: timerCounter - 1 };
+      });
+
+      console.log('o timer está rodando');
+    }, aSecondInMiliseconds);
+  };
+
+  handleOptionClick = () => {
+    this.setState({ clicked: true });
+  };
 
   shuffleAnswers = () => {
     const { results, questionIndex } = this.state;
@@ -54,7 +93,10 @@ class Game extends Component {
   };
 
   render() {
-    const { questionIndex, answersShuffled, results } = this.state;
+    const {
+      questionIndex, answersShuffled, results,
+      timerCounter, clicked, isDisabled,
+    } = this.state;
     return (
       <div className="game">
         <Header />
@@ -62,8 +104,11 @@ class Game extends Component {
           results={ results }
           answersShuffled={ answersShuffled }
           questionIndex={ questionIndex }
+          clicked={ clicked }
+          handleOptionClick={ this.handleOptionClick }
+          isDisabled={ isDisabled }
         />
-        <Timer monitoredState="" />
+        <p>{ `Timer: ${timerCounter}s` }</p>
       </div>
     );
   }
