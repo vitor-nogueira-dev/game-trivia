44be478ee0b9800dc8b6ab9f11d109388b9e9da5
Douglas Araújo <goudxs@gmail.com>
2023-02-10 18:13:17 -0300

    feat: Exibe as informações de resultado do player

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,17 +5,27 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const minAssertionsNumber = 3;
     return (
       <div className="feedback-page">
         <Header />
         <section className="feedback-message">
           {assertions >= minAssertionsNumber ? (
-            <p data-testid="feedback-text">Well Done!</p>
+            <h2 data-testid="feedback-text">Well Done!</h2>
           ) : (
-            <p data-testid="feedback-text">Could be better...</p>
+            <h2 data-testid="feedback-text">Could be better...</h2>
           )}
+          <p>
+            Você acertou
+            <span data-testid="feedback-total-question">{assertions}</span>
+            questões!
+          </p>
+          <p>
+            Um total de
+            <span data-testid="feedback-total-score">{score}</span>
+            pontos.
+          </p>
         </section>
       </div>
     );
@@ -24,10 +34,12 @@ class Feedback extends Component {
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = ({ player }) => ({
   assertions: player.assertions,
+  score: player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
