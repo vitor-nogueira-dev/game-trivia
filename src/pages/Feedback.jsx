6e9fda9f8ce968ca47cbb1466652c3667cce99d0
Douglas Araújo <goudxs@gmail.com>
2023-02-10 17:33:06 -0300

    feat: Exibe a mensagem de feedback dependendo do resultado do player

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,15 +1,33 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const minAssertionsNumber = 3;
     return (
       <div className="feedback-page">
         <Header />
+        <section className="feedback-message">
+          {assertions >= minAssertionsNumber ? (
+            <p data-testid="feedback-text">Well Done!</p>
+          ) : (
+            <p data-testid="feedback-text">Could be better...</p>
+          )}
+        </section>
       </div>
     );
   }
 }
 
-export default connect()(Feedback);
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
