59f6558cad4b5845c5061701bbdf13462a14cf25
Douglas Araújo <goudxs@gmail.com>
2023-02-11 03:37:58 -0300

    test: adiciona os testes da section de mensagens de feedback na página

--- src/tests/17.feedback.test.jsx
+++ src/tests/17.feedback.test.jsx
@@ -15,7 +15,7 @@ import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 // }
 
 describe("testa a página de feedback", () => {
-  test('Renderiza corretamente os elementos do Header tela de feedback', () => {
+  it('Renderiza corretamente os elementos do Header tela de feedback', () => {
     const initialState = {
       player: {
         name: 'John Doe',
@@ -37,5 +37,53 @@ describe("testa a página de feedback", () => {
     const playerScore = screen.getByText(/pontos:/i);
     expect(playerScore).toBeInTheDocument();
     expect(within(playerScore).getByText(/74/i)).toBeInTheDocument();
+  })
+
+  it('Renderiza corretamente a section de mensagens de feedback | Could be better...', () => {
+    const initialState = {
+      player: {
+        name: 'John Doe',
+        assertions: 2,
+        score: 74,
+        gravatarEmail: 'johndoe@test.com',
+      }
+    }
+    const route = '/feedback'
+    renderWithRouterAndRedux(<App />, initialState, route );
+
+    const feedbackText = screen.getByRole('heading', { level: 2, name: /could be better/i });
+    expect(feedbackText).toBeInTheDocument();
+
+    const assertions = screen.getByText('2');
+    const assertionsMessage = screen.getByText(/você acertou questões!/i)
+    expect(assertions).toBeInTheDocument();
+    expect(assertionsMessage).toBeInTheDocument();
+
+    const scoreMessage = screen.getByText(/um total de pontos/i)
+    expect(within(scoreMessage).getByText('74')).toBeInTheDocument();
+  })
+
+  it('Renderiza corretamente a section de mensagens de feedback | Well done!', () => {
+    const initialState = {
+      player: {
+        name: 'John Doe',
+        assertions: 4,
+        score: 112,
+        gravatarEmail: 'johndoe@test.com',
+      }
+    }
+    const route = '/feedback'
+    renderWithRouterAndRedux(<App />, initialState, route );
+
+    const feedbackText = screen.getByRole('heading', { level: 2, name: /well done/i });
+    expect(feedbackText).toBeInTheDocument();
+
+    const assertions = screen.getByText('4');
+    const assertionsMessage = screen.getByText(/você acertou questões!/i)
+    expect(assertions).toBeInTheDocument();
+    expect(assertionsMessage).toBeInTheDocument();
+
+    const scoreMessage = screen.getByText(/um total de pontos/i)
+    expect(within(scoreMessage).getByText('112')).toBeInTheDocument();
   })
 })
